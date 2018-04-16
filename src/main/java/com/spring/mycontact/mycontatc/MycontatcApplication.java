a2cfcdf972b1226fc49bb4d4b8a21524702936a7
vivhp <vihahb@gmail.com>
2018-04-16 16:27:16 +0700

    Initial commit

---
+++ src/main/java/com/spring/mycontact/mycontatc/MycontatcApplication.java
@@ -0,0 +1,12 @@
+package com.spring.mycontact.mycontatc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycontatcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycontatcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/spring/mycontact/mycontatc/MycontatcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.mycontact.mycontatc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MycontatcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
